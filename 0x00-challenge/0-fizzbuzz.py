f7e7fd2f7f33555fd5eed8ad8bcf4c3a7cdec30d
GergesBernaba1 <gergesbernaba2@gmail.com>
2024-01-28 00:29:15 -0800

    correct task 0

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,9 +1,3 @@
-#!/usr/bin/python3
-""" FizzBuzz
-"""
-import sys
-
-
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
@@ -27,13 +21,3 @@ def fizzbuzz(n):
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
-
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
-        sys.exit(1)
-
-    number = int(sys.argv[1])
-    fizzbuzz(number)
